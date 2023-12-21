dc99decabb213771433ee4e6693154df43fcec74
sahil rajput <sahilsinghsolanki2460@gmail.com>
2023-12-21 23:41:31 +0530

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,86 @@
+import React, { useEffect } from "react";
+import leetcode from "./leetcode.webp";
+import github from "./github-logo (1).png";
+import resume from "./resume.png";
+import "./Home.css";
+
+function Home() {
+  useEffect(() => {
+    const introText = document.querySelector(".intro");
+
+    const animateWords = () => {
+      const words = introText.textContent.split(" ");
+
+      introText.innerHTML = words
+        .map((word) => `<span class="word">${word}</span>`)
+        .join(" ");
+
+      let wordIndex = 0;
+      const wordElements = document.querySelectorAll(".word");
+
+      const showNextWord = () => {
+        if (wordIndex < wordElements.length) {
+          wordElements[wordIndex].classList.add("show");
+          wordIndex++;
+        } else {
+          // Reset the animation
+          wordElements.forEach((wordElement, index) => {
+            setTimeout(() => {
+              wordElement.classList.remove("show");
+            }, index * 100); // Adjust the delay as needed
+          });
+          wordIndex = 0;
+        }
+      };
+
+      setInterval(showNextWord, 1000); // Adjust the interval as needed
+    };
+
+    // Run the animation initially
+    animateWords();
+  }, []);
+
+  return (
+    <div className="home">
+      <p className="intro">Full Stack Developer ...</p>
+      <div className="description">
+        I am a Dedicated Full Stack Developer with expertise in MERN stack
+        Developement and DSA .
+        
+      </div>
+      <div className="links">
+        <ul>
+          <li>
+            <a
+              href="https://leetcode.com/sahil0605/"
+              target="/"
+              data-tooltip="LeetCode"
+            >
+              <img src={leetcode} alt="LeetCode" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/sahil0605"
+              target="/"
+              data-tooltip="GitHub"
+            >
+              <img src={github} alt="GitHub" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://docs.google.com/document/d/1HUOrRbkcPEMp4su96sPYg_bKhUSy5Poau6D8Z5bKh-c/edit?usp=sharing"
+              target="/"
+              data-tooltip="Resume"
+            >
+              <img src={resume} alt="Resume" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import logoImage from "./logo.png";
+
+function Navbar() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <Link to="/home">
+          <img src={logoImage} alt="Logo" />
+        </Link>
+      </div>
+      <div>
+        {/* Mobile menu button */}
+        <div className="mobile-menu-button" onClick={toggleMobileMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Navigation list */}
+        <ul className={`list ${isMobileMenuOpen ? "show" : ""}`}>
+          <li>
+            <Link to="/home" onClick={toggleMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/experience" onClick={toggleMobileMenu}>
+              Experience
+            </Link>
+          </li>
+          <li>
+            <Link to="/skills" onClick={toggleMobileMenu}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects" onClick={toggleMobileMenu}>
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={toggleMobileMenu}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
